d7250602bce44dc4b5293cc413759aa81591cde2
Mendez324 <48069230+Mendez324@users.noreply.github.com>
2019-09-27 17:00:04 +0100

    changed some properties and started working on form, imnot so used to do them how i thought i would

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+    $('#p1').click(function(){
+		$(".c-form__hidden").toggleClass('show');
+    });
+    
+    $('#p2').click(function(){
+		$(".c-form__hidden").toggleClass('show');
+	});
+});
